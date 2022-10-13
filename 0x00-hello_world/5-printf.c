304fee3047404ffbead98750cde0ed1c0212ebc8
the-deyemii <tellytales0@gmail.com>
2022-10-13 06:44:26 -0700

    ggg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  Displays the string specified
+ *
+ *  main - Entry point
+ *
+ *  Return value = 0
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return 0;
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** Progrm that prints size of various data types
+ *
+ * main + Entry point
+ *
+ * Return value = 0
+ */
+int main(void)
+{
+	printf("Size of a char: %c bytes\n");
+
+	printf("Size of an int: %d bytes\n");
+
+	printf("Size of a long int: %ld bytes\n");
+
+	printf("Size of a long long int: %lld bytes\n");
+
+	printf("Size of a float: %f bytes\n");
+
+	return 0;
+}
+
