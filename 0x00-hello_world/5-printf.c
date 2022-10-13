7d7522ca37d3c742135e676df563ec6a85a786be
the-deyemii <tellytales0@gmail.com>
2022-10-13 07:07:39 -0700

    ggg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- *  Displays the string specified
- *
  *  main - Entry point
- *
  *  Return value = 0
  */
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
