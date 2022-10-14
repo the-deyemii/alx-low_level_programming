fa42d6877d5cdf3a7487262b4d74c8b9718c150a
the-deyemii <tellytales0@gmail.com>
2022-10-14 11:15:06 -0700

    ggg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - print 0123456789 using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
