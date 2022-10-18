baaf2ab89eb693d3c05e2cc3de4dde038e813a30
the-deyemii <tellytales0@gmail.com>
2022-10-18 08:08:01 -0700

    ggg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Check my code
- * Return: LGBTQ+
- */
-int main(void)
-{
-	char v = 'a';
-	print_alphabet(v);
-	putchar('\n');
-	return (0);
-}
-
-/**
  * print_alphabet - prints alphabet
  *
  * Return koshi plix
  */
 
-void print_alphabet(char v)
+void print_alphabet(void)
 {
+	char v;
 	for (v = 'a'; v <= 'z'; v++)
 	{
 		putchar(v);
