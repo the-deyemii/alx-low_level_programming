29242f97b7f502012e96bc1f59d54efdc0e583e0
the-deyemii <tellytales0@gmail.com>
2022-10-18 05:03:13 -0700

    ggg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Header file
+ * Return: First one down
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	task0(c);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+void task0(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		putchar(a[i]);
+		i++;
+	}
+}
