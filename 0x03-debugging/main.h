7f9cb9428bca0cbc3280088856eff592164f8602
the-deyemii <tellytales0@gmail.com>
2022-10-19 07:05:52 -0700

    ggg

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+{
+        if (n > 0)
+        {
+                printf("%i is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%i is zero\n", n);
+        }
+        else if (n < 0)
+        {
+                printf("%i is negative\n", n);
+        }
+
+        return (0);
+}
+#endif /* MAIN_H */
