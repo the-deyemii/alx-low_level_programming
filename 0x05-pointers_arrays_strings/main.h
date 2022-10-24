901a963ca608bc42e8159334ff95b5dd69fe6e37
the-deyemii <tellytales0@gmail.com>
2022-10-24 14:25:29 -0700

    ggg

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+
+#endif
