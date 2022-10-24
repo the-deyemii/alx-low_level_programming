80868473927ac66f9f503ede31c630a3bd1cae9f
the-deyemii <tellytales0@gmail.com>
2022-10-24 16:51:35 -0700

    ggg

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,5 +7,13 @@
 void print_rev(char *s)
 {
 
-	strrev(*s);
+
+    int i, len, temp;
+    len = strlen(s); 
+    for (i = 0; i < len/2; i++)
+    {
+        temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+    }
 }
