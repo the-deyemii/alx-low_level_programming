d0be48c6103ae0c8555423720a732c159aec7a0c
the-deyemii <tellytales0@gmail.com>
2022-10-25 02:41:58 -0700

    ggg

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,21 +7,21 @@ void rev_string(char *s)
 {
 	int len, i;
 	char *start, *end, temp;
-	len = strlen (s);
+	
+	len = strlen(s);
 	start = s;
 	end = s;
-	
 	for (i = 0; i < len - 1; i++)
 	{
 		end++;
 	}
-	for (i = 0; i < len/2; i++)
-    {
+	for (i = 0; i < len / 2; i++)
+	{
         temp = *end;
         *end = *start;
         *start = temp;
 
         start++;
         end--;
-    }
+	}
 }
