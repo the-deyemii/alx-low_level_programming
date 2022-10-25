2b96f78e8fb1cd25e6dd76a66865387fe21a5f7b
the-deyemii <tellytales0@gmail.com>
2022-10-25 02:34:39 -0700

    ggg

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,21 @@
 #include <string.h>
 /**
- * print_rev - function
+ * rev_string - function
  * @s: parameter
  */
 void rev_string(char *s)
 {
-	  int len, i;
-    char *start, *end, temp;
-
-    len = strlen (s);
-    start = s;
-    end = s;
-
-    for (i = 0; i < len - 1; i++)
-    end++;
-
-    for (i = 0; i < len/2; i++)
+	int len, i;
+	char *start, *end, temp;
+	len = strlen (s);
+	start = s;
+	end = s;
+	
+	for (i = 0; i < len - 1; i++)
+	{
+		end++;
+	}
+	for (i = 0; i < len/2; i++)
     {
         temp = *end;
         *end = *start;
