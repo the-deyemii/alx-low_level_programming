b7152cf2ce9fa5dcf652f53ceca01f9b3fa8f445
the-deyemii <tellytales0@gmail.com>
2022-10-28 14:56:03 -0700

    ggg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * 2-print_alphabet_x10 - print alphabets 20 times
+ * print_alphabet_x10 - print alphabets 20 times
  * Return: iya yin
  */
 
 void print_alphabet_x10(void)
 {
-    int x = 0, y;
-
-    while (x <10)
-    {
-        y = 'a';
-
-        while (y <= 'z')
-        {
-        _putchar(y);
-        y++;
-        }
-        x++;
-        _putchar('\n');
-    }
+	int x = 0, y;
+	
+	while (x <10)
+	{
+		y = 'a';
+		
+		while (y <= 'z')
+		{
+			_putchar(y);
+			y++;
+		}
+		x++;
+		_putchar('\n');
+	}
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower(char c) - checkd for lowercase letters
+ * Return: koni dafun yin
+ */
+
+int _islower(int c)
+{
+	c = 'a';
+
+	if (c <= 'z')
+	{
+		_putchar('1');
+	}
+	else 
+	{
+		_putchar('0');
+	}
+}
