2347e4e2888691152d9fc6cf0a1f751faeda30db
the-deyemii <tellytales0@gmail.com>
2022-10-28 14:34:15 -0700

    ggg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@
 void print_alphabet(void)
 {
 	char x = 'a';
-	
+
 	while (x <= 'z')
 	{
 		_putchar(x);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 /**
  * print_alphabet - function to print alphabet
  *
