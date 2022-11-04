a7b8a29c7122a5e6da5df5cdaa31947b32088f0f
the-deyemii <tellytales0@gmail.com>
2022-11-04 10:08:45 -0700

    ggg

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - Don't delete _putchar, its used for testing
+ * @c: character to be printed
+ *
+ * Description: It doesn't affect compilation or checker testing but will crash
+ * the ALX tool tester if removed.
+ * Return: 0 (Success) or 1 (Failure).
+ */
+int _putchar(char c);
+#endif
