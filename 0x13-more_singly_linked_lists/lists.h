a20d962ecc0937cad53239fa4da95074c7e134ec
the-deyemii <tellytales0@gmail.com>
2022-11-29 19:07:07 +0000

    ggg

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,6 +12,7 @@
  * the ALX tool tester if removed.
  * Return: 0 (Success) or 1 (Failure).
  */
+int _putchar(char c);
 
 /**
  * struct listint_s - singly linked list
@@ -25,7 +26,7 @@ typedef struct listint_s
 int n;
 struct listint_s *next;
 } listint_t;
-int _putchar(char c);
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
