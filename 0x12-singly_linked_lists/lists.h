9b68880bf2687d797bc15913f8724d3d7c20390c
the-deyemii <tellytales0@gmail.com>
2022-11-29 09:19:49 +0000

    ggg

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,7 +10,7 @@
  */
 
 /**
- * list_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
